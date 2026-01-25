707157b29f7a1657fab7a8854bdcf0200f0c4437
ismaelbetancourt-jesus <ismael.betancourt@rgvsoluciones.com>
2026-01-24 17:31:39 -0700

    Adding expense report by category

--- static/sql/dashboard_queries/gastos_por_categoria.sql
+++ static/sql/dashboard_queries/gastos_por_categoria.sql
@@ -4,6 +4,5 @@ SELECT
 FROM gasto g
 JOIN categoria_gasto c ON g.id_categoria = c.id
 WHERE g.estatus != 'Cancelado'
-  AND g.fecha BETWEEN :fecha_inicio AND :fecha_fin
 GROUP BY c.nombre
 ORDER BY total DESC;
